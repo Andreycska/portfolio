42fec772427d6e29e3ad5c303662d91ce0e43e61
Andreycska <pashenkoandrew@gmail.com>
2020-12-24 18:10:41 +0300

    create fixed menu, smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,55 @@
 $(function(){
 
+    // FIXED MENU 
+
+    let header = $("#header"); //Позиция header 
+    let intro = $("#intro"); // Позиция intro
+    let introH = intro.innerHeight(); // Высота интро
+    let scrollPos = $(window).scrollTop(); // Сколько проскроли от интро
+
+    checkScroll(introH,scrollPos); //Вызываем функцию проверки где скролл
+
+    $(window).on("scroll resize", function(){ // когда происходит скролл
+        introH = intro.innerHeight(); // смотрим какое сейчас растояние от интро
+        scrollPos = $(this).scrollTop(); // и вычесляем это значение
+
+        checkScroll(introH,scrollPos); // вызываем функцию
+    });
+
+    function checkScroll (introH,scrollPos) { // функция 
+        if (scrollPos > introH) {  // если позиция скрола больше чем высота интро
+            header.addClass("fixed"); // тогда header фиксируется
+        }else {
+            header.removeClass("fixed"); // если нет то отменяем фиксацию
+        }
+    }
+
+    // SMOOTH SCROLL
+
+    $("[data-scroll]").on("click", function(event){ //по клику на элемент меню
+        event.preventDefault(); // отменяем стандартное поведение ссылки
+        let elementId = $(this).data("scroll"); // Смотрим по какому элементу меню точно шелкнули
+        let elementOffset = $(elementId).offset().top; // куда ехать
+
+        nav.removeClass("show"); // убираем класс
+        
+        $("html, body").animate({ // анимацию движения
+            scrollTop: elementOffset - 100 // едим куда - 70 пикселей
+        }, 700); // едим с какой скоростью
+    });
+
+    // HOME SMOOTH SCROLL
+
+    $("[data-home]").on("click", function(event) {
+        event.preventDefault();
+        let blockId = $(this).data("home");
+        let blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 700)
+    });
+
     // BURGER MENU
 
     const navToggle = $("#navToggle"); //кнопка бургер меню
